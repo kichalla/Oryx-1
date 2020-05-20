c94c4c0054a1d1e8ff308284ad5021433a2b76f5
Kiran Challa <kichalla@microsoft.com>
2020-05-20 11:24:46 -0700

    updated

---
+++ build/__detectorNugetPackagesVersions.sh
@@ -0,0 +1,4 @@
+# This file was auto-generated from 'constants.yaml'. Changes may be overridden.
+
+VERSION_PREFIX='1.0.0'
+IS_PRERELEASE='true'
---
+++ src/BuildScriptGenerator/Hugo/DetectorNugetPackagesVersions.cs
@@ -0,0 +1,10 @@
+// This file was auto-generated from 'constants.yaml'. Changes may be overridden.
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Hugo
+{
+    public static class DetectorNugetPackagesVersions
+    {
+        public const string VersionPrefix = "1.0.0";
+        public const string IsPrerelease = "true";
+    }
+}
