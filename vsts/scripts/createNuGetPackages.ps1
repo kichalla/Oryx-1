8c6e5ed1b3d17a4e0789800b2f9e833b8f2394f9
Kiran Challa <kichalla@microsoft.com>
2020-05-20 09:12:49 -0700

    updated

--- vsts/scripts/createNuGetPackages.ps1
+++ vsts/scripts/createNuGetPackages.ps1
@@ -8,25 +8,25 @@ function DeleteItem($pathToRemove) {
         Remove-Item -Recurse -Force -Path "$pathToRemove"
     }
 }
-$version="1.0.0-$env:BUILD_BUILDNUMBER"
-$detectorName="Microsoft.Oryx.Detector"
+
 $repoRoot="$PSScriptRoot\..\.."
 $artifactsPackagesDir="$repoRoot\artifacts\packages"
-
+. $repoRoot\build\__detectorNugetPackagesVersions.ps1
+$detectorName="Microsoft.Oryx.Detector"
 cd "$artifactsPackagesDir"
 
 # Delete any existing directory and zip file. Could have been from an earlier build.
 DeleteItem "$detectorName"
 DeleteItem "$detectorName.zip"
-Rename-Item -Path "$detectorName.$version.nupkg" -NewName "$detectorName.zip"
+Rename-Item -Path "$detectorName.$VERSION.nupkg" -NewName "$detectorName.zip"
 Expand-Archive -Path "$detectorName.zip" -DestinationPath "$detectorName"
 DeleteItem "$detectorName.zip"
 
 Copy-Item `
     -Path "$repoRoot\src\Detector\bin\Release\netstandard2.0\$detectorName.dll" `
-    -Destination "$buildScriptGeneratorCliName\lib\netstandard2.0\$detectorName.dll" `
+    -Destination "$detectorName\lib\netstandard2.0\$detectorName.dll" `
     -Force
 
 Compress-Archive -Path "$detectorName\*" -DestinationPath "$detectorName.zip"
-Rename-Item -Path "$detectorName.zip" -NewName "$detectorName.$version.nupkg"
+Rename-Item -Path "$detectorName.zip" -NewName "$detectorName.$VERSION.nupkg"
 DeleteItem "$detectorName"
