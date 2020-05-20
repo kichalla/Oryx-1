7c1f5d1ebb9f326ee2ddbf7975932bc083846cd4
Kiran Challa <kichalla@microsoft.com>
2020-05-20 14:03:55 -0700

    updated

--- build/tools/SharedCodeGenerator/Outputs/PowershellOutput.cs
+++ build/tools/SharedCodeGenerator/Outputs/PowershellOutput.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Oryx.SharedCodeGenerator.Outputs
             foreach (var constant in _collection.Constants)
             {
                 var name = constant.Key.Replace(ConstantCollection.NameSeparator[0], '_').ToUpper();
-                body.AppendLine($"${name}='{constant.Value}'");
+                body.AppendLine($"${name}=\"{constant.Value}\"");
             }
 
             return body.ToString();
